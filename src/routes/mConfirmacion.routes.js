d746d1d1c89640d7bc9fdf3eb9a3b1cfaddb2242
Cristian Velazquez <109715719+cmk95R@users.noreply.github.com>
2024-10-14 16:05:23 -0300

    se elimino archivos obsoletos

--- src/routes/mConfirmacion.routes.js
+++
@@ -1,7 +0,0 @@
-// import { Router } from "express";
-// import { mConfirmacion } from "../controllers/mConfirmacion.js";
-// const router = Router();
-
-// router.post('/mConfirmacion', mConfirmacion);
-
-// export default router
--- src/routes/mReestablecer.routes.js
+++
@@ -1,7 +0,0 @@
-// import { Router } from "express";
-// import { mReestablecer } from "../controllers/mReestablecer.js";
-// const router = Router();
-
-// router.post('/mReestablecer', mReestablecer);
-
-// export default router
